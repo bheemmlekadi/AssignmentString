bac3ba47233efbfdcdff82e9874657efafd6ed5b
Bheem Reddy Malekadi <85881511+bheemmlekadi@users.noreply.github.com>
2023-03-03 21:22:43 +0530

    Add files via upload

---
+++ count vowels consonants special chars.java
@@ -0,0 +1,34 @@
+package assignment4;
+
+public class countVowelsConsonants {
+  public static void main(String[] args) {
+     String line = "This website is aw3som3.";
+     int vowels = 0, consonants = 0,  spaces = 0;
+      line = line.toLowerCase();
+		    for (int i = 0; i < line.length(); ++i) {
+		      char ch = line.charAt(i);
+
+		      // check if character is any of a, e, i, o, u
+		      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+		        ++vowels;
+		      }
+
+		      // check if character is in between a to z
+		      else if ((ch >= 'a' && ch <= 'z')) {
+		        ++consonants;
+		      }
+		      
+		  
+		      
+		      // check if character is a white space
+		      else if (ch == ' ') {
+		        ++spaces;
+		      }
+		    }
+
+		    System.out.println("Vowels: " + vowels);
+		    System.out.println("Consonants: " + consonants);
+		    System.out.println("White spaces: " + spaces);
+		  }
+		}   
+  
---
+++ pallindrome program.java
@@ -0,0 +1,19 @@
+package assignment4;
+
+public class checkPallindrome {
+	 public static void main(String args[]){  
+	  int r,sum=0,temp;    
+	  int n=2552;//It is the number variable to be checked for palindrome  
+	  
+	  temp=n;    
+	  while(n>0){    
+	   r=n%10;  //getting remainder  
+	   sum=(sum*10)+r;    
+	   n=n/10;    
+	  }    
+	  if(temp==sum)    
+	   System.out.println("palindrome number ");    
+	  else    
+	   System.out.println("not palindrome");    
+	}  
+	}  
